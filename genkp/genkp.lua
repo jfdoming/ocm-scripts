0e42591e467df6eb62b1a03fc7983f11198dc013
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2021-02-10 02:49:01 -0500

    Add KP generator

---
+++ genkp/genkp.lua
@@ -0,0 +1,17 @@
+files = require("ocmutils.files")
+
+if files.isPlainFile(files.PRKEY_PATH) then
+    io.stderr:write("Keypair already exists for this computer.\n")
+    return 1
+end
+
+local pubkey, prkey = component.data.generateKeyPair()
+if not files.writeBinary(files.PUBKEY_PATH, pubkey.serialize()) then
+    io.stderr:write("Failed to write public key.\n")
+    return 1
+end
+
+if not files.writeBinary(files.PRKEY_PATH, prkey.serialize()) then
+    io.stderr:write("Failed to write private key.\n")
+    return 1
+end
