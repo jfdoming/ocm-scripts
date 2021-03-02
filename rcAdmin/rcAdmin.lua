8d8d50aa900e9fc87f2509f4a8a6ffa2649d9d1c
Julian Frank Dominguez-Schatz <dolphinspray.fr@gmail.com>
2021-03-02 04:25:08 -0500

    Add new "rcAdmin" utility for updating rc services

---
+++ rcAdmin/rcAdmin.lua
@@ -0,0 +1,28 @@
+local COMMANDS = {
+    ["update"] = "rcAdmin.update",
+}
+
+local commandKeys = {}
+for k, _ in pairs(COMMANDS) do
+    commandKeys[#commandKeys + 1] = k
+end
+local COMMAND_LIST = table.concat(commandKeys, ", ")
+
+
+arg = {...}
+
+if #arg == 0 or arg[1] == "help" or COMMANDS[arg[1]] == nil then
+    print("Usage: rcAdmin [command]")
+    print("Where command is one of: " .. COMMAND_LIST)
+    return 0
+end
+
+local status, command = pcall(require, COMMANDS[arg[1]])
+if not status or command == nil then
+    io.stderr:write("Failed to run that command!\n")
+    return 1
+end
+
+--- Skip the command name and run.
+table.remove(arg, 1)
+command(arg)
